2ef9ae5e72609d86132874443e4ae46447afaa5e
Jamal Moir <jamal.moir@gmail.com>
2016-03-06 18:23:25 +0900

    Add class to represent a post

---
+++ pyblogit/posts.py
@@ -0,0 +1,48 @@
+"""
+pyblogit.posts
+~~~~~~~~~~~~~~
+
+This module contains the data model to represent blog posts and methods
+to manipulate it.
+"""
+
+class post(object):
+
+    def __init__(self, post_id, title, url, author, content, images, labels,
+            status):
+        self._post_id = post_id
+        self._title = title
+        self._url = url
+        self._author = author
+        self._content = content
+        self._images = images
+        self._labels = labels
+
+    @property
+    def post_id(self):
+        return self._post_id
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def content(self):
+        return self._content
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
