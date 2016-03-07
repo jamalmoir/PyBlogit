45ebf89018512f93592083cf0cf0413d4f8c6d4a
Jamal Moir <jamal.moir@gmail.com>
2016-03-07 16:37:41 +0900

    Add blogger authentication

--- pyblogit/api_interface.py
+++ pyblogit/api_interface.py
@@ -6,36 +6,52 @@ This modules acts as an interface between pyblogit and various
 blogging platform apis.
 """
 
-import gdata.gauth
-import gdata.blogger.client
+import oauth2client.client
+import oauth2client.file
+import httplib2
 
 class BloggerInterface(object):
+    """Connects to blogger api and authorises client."""
 
     def __init__(self):
-        self._CLIENT_ID = client_id
-        self._CLIENT_SECRET = client_secret
-        self._SCOPE = 'https://www.googleapis.com/auth/blogger'
+        #TODO
 
     def get_access_code(self):
         """Opens dafualt browser to the google auth page and provides
-        them with an access code."""
-        token = gdata.gauth.OAuth2Token(
-                client_id = self.CLIENT_ID,
-                client_secret = self.CLIENT_SECRET,
-                scope = self.SCOPE,
-                user_agent = 'pyblogit')
+        them with an authorisation code."""
+        flow = oauth2client.client.OAuth2WebServerFlow(client_id,
+                client_secret, scope)
 
-        url = token.generate_authorize_url(redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+        auth_uri = flow.step1_get_authorize_url()
 
-        webbrowser.open_new_tab(url)
+        webbrowser.open_new_tab(auth_uri)
 
-    def generate_token(self, code):
-        """Generates new api access token."""
-        self._token = token.get_access_token(code)
+    def get_credentials(self):
+        """Gets google api credentials, or generates new credentials
+        if they don't exist or are invalid."""
+        storage = oauth2client.file.Storage('credentials.dat')
+        credentials = storage.get()
 
-    def get_client(self):
-        """Returns an authorised blogger api client."""
-        client = gdata.blogger.client.BloggerClient()
-        self._token.authorize(client)
+        if not credentials or credientials.invalid:
+            credentials = tools.run_flow(flow, storage,
+                    tools.argparser.parse_args())
 
-        return client
+        return credentials
+
+    def get_service(self):
+        """Returns an authorised blogger api service."""
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+        service = build('blogger', 'v3', http=http)
+
+        return service
+
+def get_client():
+    interface = BloggerInterface()
+    client = interface.get_client()
+
+    return client
+
+def get_blog(url):
+    client = get_client()
+    
