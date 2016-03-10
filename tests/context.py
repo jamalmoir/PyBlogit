0bfb417e05673fad981c232115c86ce6f8b33124
Jamal Moir <jamal.moir@gmail.com>
2016-03-10 16:23:27 +0900

    Update testing files

--- tests/context.py
+++ tests/context.py
@@ -4,4 +4,4 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import pyblogit
-import pyblogit.api_interface
+import pyblogit.database_handler
---
+++ tests/test_local_database.py
@@ -0,0 +1,2 @@
+import pytest
+from .context import pyblogit.database_handler
