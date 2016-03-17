de0393eb503eb17d5c9d249d82aaac9ff272aea2
Jamal Moir <jamal.moir@gmail.com>
2016-03-17 11:32:10 +0900

    Add get_service() test

--- tests/test_api_interface.py
+++ tests/test_api_interface.py
@@ -1,4 +1,5 @@
 from .context import pyblogit
+import pyblogit.api_interface
 import pytest
 import unittest.mock
 
@@ -14,10 +15,10 @@ def test_get_credentials():
         credentials = storage.get.return_value
         credentials.invalid = True
 
-        # run the method and see if we get what we want.
+        # Run the method and see if we get what we want.
         result = pyblogit.api_interface.BloggerInterface().get_credentials()
 
-        # check that the flow was initialised correctly.
+        # Check that the flow was initialised correctly.
         mock_flow.assert_called_with('client_secret.json',
                                      'https://www.googleapis.com/auth/blogger',
                                      redirect_uri='urn:ietf:wg:oauth:2.0:oob')
@@ -27,11 +28,35 @@ def test_get_credentials():
         # url from the flow and pass it to the browser. Then the
         # authentication code should be taken from input and passed
         # back to the flow for exchange. Test these interactions
-        # took place
+        # took place.
         flow = mock_flow.return_value
+
         flow.step1_get_authorize_url.assert_called_once_with()
         mock_wbopen.assert_called_once_with(
             flow.step1_get_authorize_url.return_value)
         flow.step2_exchange.assert_called_once_with(mock_input.return_value)
         storage.put(flow.step2_exchange.return_value)
+
         assert result == flow.step2_exchange.return_value
+
+
+def test_get_service():
+    with unittest.mock.patch('pyblogit.api_interface.BloggerInterface.get_credentials') as mock_blogger, \
+            unittest.mock.patch('httplib2.Http') as mock_http, \
+            unittest.mock.patch('apiclient.discovery.build') as mock_api:
+
+        # Run the method to see if we get what we want.
+        result = pyblogit.api_interface.BloggerInterface().get_service()
+
+        # Credentials should be retrieved and used to authorise an
+        # http2lib.Http object. This is then used to build an
+        # API client. Test these interactions too place.
+        credentials = mock_blogger.return_value
+
+        mock_blogger.assert_called_once_with()
+        mock_http.assert_called_once_with()
+        credentials.authorize.assert_called_once_with(mock_http.return_value)
+        mock_api.assert_called_once_with('blogger', 'v3',
+                                         http=credentials.authorize.return_value)
+
+        assert result == mock_api.return_value
